e05ec40aecdc588304efb0671aa5f684806c5bd7
DESKTOP-49C2M5J\LEE <leechang0423@naver.com>
2019-02-19 23:32:35 +0900

    4. 핸들러 인터셉터

--- src/main/java/me/anpan/anpanspringwebmvc/web/config/WebConfig.java
+++ src/main/java/me/anpan/anpanspringwebmvc/web/config/WebConfig.java
@@ -1,14 +1,16 @@
 package me.anpan.anpanspringwebmvc.web.config;
 
+import me.anpan.anpanspringwebmvc.web.interceptor.AnotherIntercepter;
+import me.anpan.anpanspringwebmvc.web.interceptor.GreetingIntercepter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-
     @Override
-    public void addFormatters(FormatterRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new GreetingIntercepter()).order(0);
+        registry.addInterceptor(new AnotherIntercepter()).order(-1).addPathPatterns("/hi");
     }
 }
--- src/main/java/me/anpan/anpanspringwebmvc/web/controller/SampleController.java
+++ src/main/java/me/anpan/anpanspringwebmvc/web/controller/SampleController.java
@@ -2,12 +2,20 @@ package me.anpan.anpanspringwebmvc.web.controller;
 
 import me.anpan.anpanspringwebmvc.web.dto.Person;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
+
+    //preHandle 1
+    //preHandle 2
+    //요청처리
+    //postHandler 2
+    //postHandler 1
+    //뷰 랜더링
+    //afterCompletion 2
+    //afterCompletion  1
 
     @GetMapping("/hello")
     public String hello(@RequestParam("id") Person person) {
